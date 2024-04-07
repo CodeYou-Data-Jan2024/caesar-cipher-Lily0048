5fd1e5fdd188765fb72334ad76d9c5d5196f7de7
Lily Bishop <13lilybishop13@gmail.com>
2024-04-07 16:47:57 -0400

    committed my cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,11 @@
-# add your code here
+plaintext = input("Please enter a sentence:")
+plaintext = plaintext.lower ()
+newtext = ""
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+shifted = ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"]
+for letter in plaintext:
+    if letter in alphabet:
+        newtext += shifted [alphabet.index(letter)]
+else:
+        newtext += letter
+print(newtext)
